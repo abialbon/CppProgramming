dda5c5151d2ba68900e9b65c9922024a55874ef1
Abialbon Paul <dr.abialbon@gmail.com>
2019-10-17 07:48:52 +0100

    Timing threads

--- concurrency/estimate_pi.cpp
+++ concurrency/estimate_pi.cpp
@@ -2,32 +2,50 @@
 #include<thread>
 #include<vector>
 #include<math.h>
+#include<chrono>
+
+#define N_T 8
+#define N 1'000'000
+
 
 using namespace std;
 
-void t_func(int s, long n, int count, vector<double> &t) {
-    double sum = 0;
-    for (long i = s; i < n; i = i+count) {
-        sum += (double) pow(-1, i)/(2*i + 1);
+void t_func(int i, int s, int e, vector<double> &t) {
+    double sum = 0.0;
+    double div = 0.0 + 2*s;
+    for (int i = s; i < e; i = i+2) {
+        sum = sum + 1/ (div + 1) - 1 / (div + 3);
+        div += 4.0;
     }
-    t[s] = sum;
+    t[i] = sum;
+}
+
+void pi_estimate() {
+    std::chrono::time_point<std::chrono::system_clock> start, end; 
+    start = std::chrono::system_clock::now(); 
+    vector<double> t_vals (N_T);
+    vector<thread> t_arr (N_T);
+    double pi;
+    int no_per_thread = N/N_T;
+    for (int i = 0; i < N_T; i++) {
+        t_arr[i] = (thread(t_func, i, i*no_per_thread, i*no_per_thread + no_per_thread, ref(t_vals)));
+    } 
+
+    for (int i = 0; i < N_T; i++) {
+        t_arr[i].join();
+    }
+    for (int i = 0; i < N_T; i++) {
+        pi += t_vals[i];
+    }
+    end = std::chrono::system_clock::now(); 
+    cout << "From main: " << pi*4 << endl;
+    std::chrono::duration<double> elapsed_seconds = end - start; 
+    
+  
+    std::cout << "elapsed time: " << elapsed_seconds.count() * 1000 << "s\n"; 
 }
 
 int main() {
-    int thread_no = 8;
-    vector<double> t_vals (thread_no);
-    vector<thread> t_arr (thread_no);
-    double pi;
-    for (int i = 0; i < thread_no; i++) {
-        t_arr[i] = (thread(t_func, i, 1000000000, thread_no, ref(t_vals)));
-    } 
-
-    for (int i = 0; i < thread_no; i++) {
-        t_arr[i].join();
-    }
-    for (int i = 0; i < thread_no; i++) {
-        pi += t_vals[i];
-    }
-    cout << "From main: " << 4*pi << endl;
+    pi_estimate();
     return 0;
 }
