56d593852c00d7bf6dc9033d9b8e63815e8207bb
Abialbon Paul <dr.abialbon@gmail.com>
2019-10-27 23:17:50 +0000

    Implemented divide

--- simd/pi_simd.cpp
+++ simd/pi_simd.cpp
@@ -2,13 +2,14 @@
 #include <immintrin.h>
 #include "/home/abialbon/Documents/MyProjects/learning_cpp/concurrency/Chrono.h"
 
-#define N 100'000
+#define N 1'000'000
 #define float8 __m256
 #define set8(x) _mm256_set1_ps(x)
 #define add8(a, b) _mm256_add_ps(a, b)
 #define seti(a, b, c, d, e, f, g, h) _mm256_set_ps(a, b, c, d, e, f, g, h)
 #define reci8(x) _mm256_rcp_ps(x)
 #define store8(a, b) _mm256_store_ps(a, b)
+#define div8(a, b) _mm256_div_ps(a, b)
 
 using namespace std;
 
@@ -27,14 +28,15 @@ void estimate_pi_simple_simd() {
     float8 results = set8(0.0);
     float8 base = seti(1.0, -3.0, 5.0, -7.0, 9.0, -11.0, 13.0, -15.0);
     float8 increment = seti(16.0, -16.0, 16.0, -16.0, 16.0, -16.0, 16.0, -16.0);
-    
+    float8 ones = set8(1.0);
     for(int i = 0; i < N/8; ++i) {
         if (i != 0) {
 
             base = add8(base, increment);
         }
         float8 terms = base;
-        terms = reci8(terms);
+        //terms = reci8(terms);
+        terms = div8(ones, terms);
         results = add8(terms, results);
     }
 
